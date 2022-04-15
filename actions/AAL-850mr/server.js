7c69918672c5ab08737bef2f90a01b54629d8df1
Dipen Bhikadya <dipen.bhikadya@gmail.com>
2022-04-15 21:34:00 +0530

    Automatic commit from Bubble

--- actions/AAL-850mr/server.js
+++ actions/AAL-850mr/server.js
@@ -5,13 +5,13 @@ function(properties, context) {
     const body = {
         "provider": context.keys.provider,
         "channel_key": context.keys.channel_key,
-        "to": properties.to,
+        "from": properties.from,
         "name": properties.name,
         "text": properties.text,
     };
     
     return context.async(cb => {
-        fetch("https://api.sociocs.com/message", {
+        fetch("https://api.sociocs.com/incoming", {
             method: 'post',
             body: JSON.stringify(body),
             headers: {
