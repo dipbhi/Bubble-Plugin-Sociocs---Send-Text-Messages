36bf4a515c89296d2efb7faedeb2892f76d6e045
Dipen Bhikadya <dipen.bhikadya@gmail.com>
2022-04-18 17:27:23 +0530

    Automatic commit from Bubble

--- actions/AAF-850mc/server.js
+++ actions/AAF-850mc/server.js
@@ -8,7 +8,21 @@ function(properties, context) {
         "to": properties.to,
         "name": properties.name,
         "text": properties.text,
+        "image_url": properties.image_url,
+        "file_url": properties.file_url
     };
+    
+    if(!body.text && !body.image_url && !body.file_url){
+        return "At least one of these fields is required: text, image_url, file_url.";
+    }
+    
+    if(body.image_url && body.image_url.startsWith("//")){
+        body.image_url = "https:" + body.image_url;
+    }
+    
+    if(body.file_url && body.file_url.startsWith("//")){
+        body.file_url = "https:" + body.file_url;
+    }
     
     return context.async(cb => {
         fetch("https://api.sociocs.com/message", {
@@ -24,9 +38,11 @@ function(properties, context) {
             if(response.status === "success") {
                 return cb(undefined, response);
             } else {
+                console.error(JSON.stringify(response.errors));
                 return cb(JSON.stringify(response.errors));
             }
         }).catch(err => {
+            console.error(err);
             return cb(err);
         });
     });
