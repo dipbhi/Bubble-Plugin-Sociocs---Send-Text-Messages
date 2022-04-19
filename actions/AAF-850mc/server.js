4f92cda3bf2fe7738b413c44683fc7963c247fa8
Dipen Bhikadya <dipen.bhikadya@gmail.com>
2022-04-19 13:34:59 +0530

    Automatic commit from Bubble

--- actions/AAF-850mc/server.js
+++ actions/AAF-850mc/server.js
@@ -12,8 +12,12 @@ function(properties, context) {
         "file_url": properties.file_url
     };
     
-    if(!body.text && !body.image_url && !body.file_url){
-        return "At least one of these fields is required: text, image_url, file_url.";
+    if(properties.image_urls && properties.image_urls.length()) {
+        body.image_urls = properties.image_urls.get(0, properties.image_urls.length()).map(x => x.startsWith("//") ? "https:" + x : x);
+    }
+    
+    if(!body.text && !body.image_url && !body.image_urls && !properties.image_urls.length() && !body.file_url){
+        return "At least one of these must be provided: Text, Image URL, Image URLs, File URL.";
     }
     
     if(body.image_url && body.image_url.startsWith("//")){
@@ -23,28 +27,32 @@ function(properties, context) {
     if(body.file_url && body.file_url.startsWith("//")){
         body.file_url = "https:" + body.file_url;
     }
-    
-    return context.async(cb => {
-        fetch("https://api.sociocs.com/message", {
-            method: 'post',
-            body: JSON.stringify(body),
-            headers: {
-                "Content-Type": "application/json",
-                "apikey": context.keys.api_key,
-            }
-        }).then(res => {
-            return res.json();
-        }).then(response => {
+        
+    return context.async(async cb => {
+       
+        try {
+            const res = await fetch("https://api.sociocs.com/message", {
+                method: 'post',
+                body: JSON.stringify(body),
+                headers: {
+                    "Content-Type": "application/json",
+                    "apikey": context.keys.api_key,
+                }
+            });
+
+            const response = await res.json();
+
             if(response.status === "success") {
                 return cb(undefined, response);
             } else {
                 console.error(JSON.stringify(response.errors));
                 return cb(JSON.stringify(response.errors));
             }
-        }).catch(err => {
+        } catch(err) {
             console.error(err);
             return cb(err);
-        });
+        }
+        
     });
 
 }
